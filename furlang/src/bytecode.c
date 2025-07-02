7bda7462eb59dfb33309bd6b8127a56da75392a2
RealCatDev <real.catdev@gmail.com>
2025-07-02 20:54:30 +0200

    Made runtime run fbc files

--- furlang/src/bytecode.c
+++ furlang/src/bytecode.c
@@ -21,6 +21,8 @@ void furlang_write_fbc_to_file(Fbc fbc, const char *filepath) {
   assert(fwrite(&fbc.header.globalVariableCount, 2, 1, fd) == 1);
   assert(fwrite(&fbc.bytecodeLength, 8, 1, fd) == 1);
   assert(fwrite(fbc.bytecode, fbc.bytecodeLength, 1, fd) == 1);
+
+  fclose(fd);
 }
 
 Fbc furlang_read_fbc_from_file(const char *filepath, Furlang_Allocator *allocator) {
@@ -28,17 +30,20 @@ Fbc furlang_read_fbc_from_file(const char *filepath, Furlang_Allocator *allocato
 
   Fbc fbc = {0};
 
-  FILE *fd = fopen(filepath, "wb");
+  FILE *fd = fopen(filepath, "rb");
   assert(fd);
 
-  assert(fread((uint8_t[])FBC_MAGIC, 4, 1, fd) == 1);
+  assert(fread(&fbc.header.magic, 4, 1, fd) == 1);
   assert(fread(&fbc.header.version, 4, 1, fd) == 1);
   assert(fread(&fbc.header.entryFunction, 8, 1, fd) == 1);
   assert(fread(&fbc.header.functionCount, 8, 1, fd) == 1);
+
+  fbc.header.functions = furlang_allocator_alloc(allocator, sizeof(*fbc.header.functions) * fbc.header.functionCount);
+  assert(fbc.header.functions);
   for (size_t i = 0; i < fbc.header.functionCount; ++i) {
-    Fbc_Header_Function func = fbc.header.functions[i];
-    assert(fread(&func.address, sizeof(func.address), 1, fd) == 1);
-    assert(fread(&func.paramCount, 8, 1, fd) == 1);
+    Fbc_Header_Function *func = &fbc.header.functions[i];
+    assert(fread(&func->address, sizeof(func->address), 1, fd) == 1);
+    assert(fread(&func->paramCount, 8, 1, fd) == 1);
   }
   assert(fread(&fbc.header.globalVariableCount, 2, 1, fd) == 1);
   assert(fread(&fbc.bytecodeLength, 8, 1, fd) == 1);
--- runtime/main.c
+++ runtime/main.c
@@ -1,61 +1,17 @@
 #include <furlang/furlang.h>
 #include <furlang/bytecode.h>
 
+#include <stdio.h>
 #include <assert.h>
 
-int main(void) {
-  char bytecode[] = {
-         // main() - 0:
-/*  0 */ FURLANG_INSTRUCTION_BIPUSH, 20,
-/*  2 */ FURLANG_INSTRUCTION_SFPUSH, 0, 1,
-/*  5 */ FURLANG_INSTRUCTION_CALL,
-/*  6 */ FURLANG_INSTRUCTION_RET,
-/*  7 */ // fib(n) - 1:
-/*  7 */ FURLANG_INSTRUCTION_BIPUSH, 0,
-/*  9 */ FURLANG_INSTRUCTION_STR, 0, 1,
-/* 12 */ FURLANG_INSTRUCTION_BIPUSH, 1,
-/* 14 */ FURLANG_INSTRUCTION_STR, 0, 2,
-/* 17 */ // loop:
-/* 17 */ FURLANG_INSTRUCTION_LOD, 0, 0,
-/* 20 */ FURLANG_INSTRUCTION_JZ, FURLANG_BYTECODE_ADDR(54 /* end */),
-/* 28 */ FURLANG_INSTRUCTION_LOD, 0, 1,
-/* 31 */ FURLANG_INSTRUCTION_DUP,
-/* 32 */ FURLANG_INSTRUCTION_LOD, 0, 2,
-/* 35 */ FURLANG_INSTRUCTION_ADD,
-/* 36 */ FURLANG_INSTRUCTION_LOD, 0, 2,
-/* 39 */ FURLANG_INSTRUCTION_STR, 0, 1,
-/* 42 */ FURLANG_INSTRUCTION_STR, 0, 2,
-/* 45 */ FURLANG_INSTRUCTION_LOD, 0, 0,
-/* 48 */ FURLANG_INSTRUCTION_BIPUSH, 1,
-/* 50 */ FURLANG_INSTRUCTION_SUB,
-/* 51 */ FURLANG_INSTRUCTION_STR, 0, 0,
-/* 54 */ // end:
-/* 54 */ FURLANG_INSTRUCTION_RET
-  };
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    fprintf(stderr, "Usage: %s <input.fbc>\n", argv[0]);
+    return 1;
+  }
 
-  Furlang_Context context = furlang_context_create((Fbc){
-    .header = {
-      .magic = FBC_MAGIC,
-      .version = 0,
-      .entryFunction = 0,
-      .functionCount = 2,
-      .functions = (Fbc_Header_Function[]){
-        (Fbc_Header_Function){
-          .address = 0,
-          .paramCount = 0
-        },
-        (Fbc_Header_Function){
-          .address = 7,
-          .paramCount = 1
-        }
-      }
-    },
-    .bytecodeLength = sizeof(bytecode)/sizeof(*bytecode),
-    .bytecode = bytecode
-  }, NULL);
-
+  Furlang_Context context = furlang_context_create(furlang_read_fbc_from_file(argv[1], NULL), NULL);
   furlang_context_run(context);
-
   furlang_context_destroy(context, NULL);
 
   return 0;
